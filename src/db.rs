d20f370ac04e7ff6e1d1dc66cd906b87544293f0
Natalie Bridgers <nat@natalie.sh>
2022-03-23 17:02:26 -0500

    docker support (ish?)

--- src/db.rs
+++ src/db.rs
@@ -2,15 +2,16 @@ use sqlx::postgres::{PgPool, PgPoolOptions};
 use std::env;
 
 pub async fn get_pool() -> anyhow::Result<PgPool, anyhow::Error> {
+    println!(
+        "Connecting to the database at url {}",
+        &env::var("DATABASE_URL")?
+    );
     let pool = PgPoolOptions::new()
         .max_connections(50)
         .min_connections(1)
         .max_lifetime(std::time::Duration::from_secs(10))
         .connect(&env::var("DATABASE_URL")?)
         .await?;
-    println!(
-        "Connected to the database at url {}",
-        &env::var("DATABASE_URL")?
-    );
+    println!("Connected");
     Ok(pool)
 }
